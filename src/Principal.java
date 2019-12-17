6e93819267c843dcb12acd85b02370e64791238e
Iker <infimezcorta@elorrieta-errekamari.com>
2019-12-17 16:26:39 +0100

    Cambio Iker carta

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Este es programa principal
+		
+		Carta cartaIker = new Carta("Bastos", 4);
 	}
 
 }
